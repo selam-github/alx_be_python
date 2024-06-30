b787bdaf08d4fd29080cfa1baba052ea0966f5ec
selamawit Haile <selamhi18@gmail.com>
2024-06-30 14:14:43 -0700

    mulitiplication calculatoer

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,4 @@
-number=int(input("Enter a number to see its multiplication table:"))
-y=(1,2,3,4,5,6,7,8,9,10)
-x=number 
-for y in range(1,11):
-        z= number * y
-            print(f"{x}  *  {y}  ={z}")
+number = int(input("Enter a number to see its multiplication table: "))
+for i in range(1, 11):
+        product = number * i
+            print(f"{number} * {i} = {product}")
