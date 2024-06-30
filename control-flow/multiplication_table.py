e0f4702135cb9da2f36f35ad3cc0bd45e20b4ad4
selamawit Haile <selamhi18@gmail.com>
2024-06-30 13:04:48 -0700

    update mulitipilication

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number to see its multiplication table:"))
+y=(1,2,3,4,5,6,7,8,9,10)
+x=number 
+for y in range(1,11):
+        z= number * y
+            print(f"{x}  *  {y}  ={z}")
