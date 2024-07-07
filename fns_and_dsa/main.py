744146fa876901bf7333bd410d4ac86b28ee387a
selamawit Haile <selamhi18@gmail.com>
2024-07-06 17:41:30 -0700

    updated arithmetic operation

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,21 +1,25 @@
 # main.py
 
 from arithmetic_operations import perform_operation
+ print("Arithmetic Operations")
+ num1 = float(input("Enter the first number: "))
+ num2 = float(input("Enter the second number: "))
+ operation = input("Enter the operation (add, subtract, multiply, divide): ")
 # Example usage:
 num1 = 5.0
 num2 = 6.0
 # Addition
 result_add = perform_operation(num1, num2, 'add')
-    print(f"Addition result: {result_add}")
+    print(f"Result: {result_add}")
 # Subtraction
 result_subtract = perform_operation(num1, num2, 'subtract')
-    print(f"Subtraction result: {result_subtract}")
+    print(f"result: {result_subtract}")
 # Multiplication
 result_multiply = perform_operation(num1, num2, 'multiply')
-    print(f"Multiplication result: {result_multiply}")
+    print(f"result: {result_multiply}")
 # Division
 result_divide = perform_operation(num1, num2, 'divide')
 if isinstance(result_divide, float):
-        print(f"Division result: {result_divide}")
+        print(f"result: {result_divide}")
     else:
-            print(f"Division error: {result_divide}")
+            print(f" error: {result_divide}")
