c3e7936f287082f56663e4231c693a95c46fa7db
selamawit Haile <selamhi18@gmail.com>
2024-07-10 12:00:19 -0700

    update bank account

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -14,7 +14,7 @@ def main():
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
-        print(f"Deposited: ${amount}")
+        print(f"Deposited: ${amount:.1f}")
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
             print(f"Withdrew: ${amount}")
