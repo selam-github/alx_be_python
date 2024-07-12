36f31d2839bbe9abccc5438d678a9b83afae513f
selamawit Haile <selamhi18@gmail.com>
2024-07-11 21:49:08 -0700

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,18 @@
 #bank_account.py
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+        self.balance = initial_balance
 
     def deposit(self, amount):
         if amount > 0:
-            self.__account_balance += amount
+            self.balance  += amount
             print(f"Deposited:  ${amount:.1f}")
         else:
             print("Invalid deposit amount. Amount must be positive.")
 
     def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if 0 < amount <= self.balance:
+            self.balance -= amount
             print(f"Withdrew:  ${amount}")
             return True
         else:
