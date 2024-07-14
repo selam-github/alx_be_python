d1ad6fe2137c3d2bec09c9de79334260455601c8
selamawit Haile <selamhi18@gmail.com>
2024-07-14 10:19:19 -0700

    unittest for calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,20 +13,20 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-2,-2), -4)
     def test_subtraction(self):
-        self.assertEqual(self.calc.sub(5,2), 3)
-        self.assertEqual(self.calc.sub(2,3), -1)
-        self.assertEqual(self.calc.sub(-5,2),-7)
-        self.assertEqual(self.calc.sub(5,-2), 7)
+        self.assertEqual(self.calc.subtract(5,2), 3)
+        self.assertEqual(self.calc.subtract(2,3), -1)
+        self.assertEqual(self.calc.subtract(-5,-2),-7)
+        self.assertEqual(self.calc.subtract(5,-2), 7)
     def test_multiplication(self)
-        self.assertEqual(self.calc.mup(3,5), 15)
-        self.assertEqual(self.calc.mup(-6,2), -12)
-        self.assertEqual(self.calc.mup(3,-5), -15)
-        self.assertEqual(self.calc.mup(-7,-3), 21)
+        self.assertEqual(self.calc.multiply(3,5), 15)
+        self.assertEqual(self.calc.multiply((-6,2), -12)
+        self.assertEqual(self.calc.multiply((3,-5), -15)
+        self.assertEqual(self.calc.multiply((-7,-3), 21)
     def test_division(self)
-        self.assertEqual(self.calc.div(6,3), 2)
-        self.assertEqual(self.calc.div(-9,3), -3)
-        self.assertEqual(self.calc.div(-15,-3), 5)
-        self.assertEqual(self.calc.div(8,0), undifned):
+        self.assertEqual(self.calc.divide(6,3), 2)
+        self.assertEqual(self.calc.divide(-9,3), -3)
+        self.assertEqual(self.calc.divide(-15,-3), 5)
+        self.assertEqual(self.calc.divide(8,0), undifned):
 
 
 
