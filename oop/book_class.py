84327d0b1a391711991dd02a85536bf31e49dc99
selamawit Haile <selamhi18@gmail.com>
2024-07-19 13:44:58 -0700

    book class updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,10 +3,10 @@ class Book:
     def __init__(self, title, author,year ):
      self.title = title
      self.author = author
-     self.year =year
-    def __del__(self):
-      return f"Deleting { self.title}"
+     self.year = year
     def __str__(self):
       return f" {self.title} by {self.author} ,published in  {self.year}"
     def __repr__(self):
-      return f" Book('{self.title}',{self.author},{self.year})
+      return f"Book('{self.title}','{self.author}',{self.year})"
+    def __del__(self)
+      return f"deleting {self.title}"
