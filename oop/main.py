5c260ea2c7828b445c51b699859d021906bb3963
selamawit Haile <selamhi18@gmail.com>
2024-07-19 09:52:09 -0700

    update book class

--- oop/main.py
+++ oop/main.py
@@ -9,3 +9,5 @@ def main():
   print(f"{my_book.title} by {my_book.author}, published in {my_book.year}")
   # Deleting a book instance to trigger __del_
   my_book.__del__(title)
+  if __name__ == "__main__":
+    main()
