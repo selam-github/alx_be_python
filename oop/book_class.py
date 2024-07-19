e6477d41cfa160724580788880c6857ea7e52c1f
selamawit Haile <selamhi18@gmail.com>
2024-07-19 05:27:22 -0700

    Book class

---
+++ oop/book_class.py
@@ -0,0 +1,12 @@
+#library_system.py
+class Book:
+def __init__(self, title, author,year)
+     self.title = title
+     self.author = author
+     self.year =year
+def __del__(self):
+    return f"Deleting { self.title}"
+def __str__(self):
+  return f"{self.title}by{self.author},{self.year}
+def__repr(self):
+    return f"{title=self.title}by{author=self.author},published{year=self.year}"
