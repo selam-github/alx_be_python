b02b348c225bb0862bf4761125626b3f934c3980
selamawit Haile <selamhi18@gmail.com>
2024-07-19 13:46:26 -0700

    book class updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,5 +8,5 @@ class Book:
       return f" {self.title} by {self.author} ,published in  {self.year}"
     def __repr__(self):
       return f"Book('{self.title}','{self.author}',{self.year})"
-    def __del__(self)
+    def __del__(self):
       return f"deleting {self.title}"
