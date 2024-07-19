33be5195155513a235c99398a2bc82d010982b7d
selamawit Haile <selamhi18@gmail.com>
2024-07-19 05:35:18 -0700

    book class main file

---
+++ oop/main.py
@@ -0,0 +1,11 @@
+#main.py
+from Book_class import BOOK
+  def main()
+  #creating instance of book
+  my_book =Book(1984,"Georg Orwell",1949)
+  # Demonstrating the __str__ method
+  print(__str__(my_book))
+  # Demonstrating the __repr__ method
+  print(__repr__(my_book))
+  # Deleting a book instance to trigger __del_
+   my_book.__del__(title)
