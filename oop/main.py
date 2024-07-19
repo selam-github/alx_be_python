2d8c3e206e43befa02eed770953d7b014c175587
selamawit Haile <selamhi18@gmail.com>
2024-07-19 06:30:54 -0700

    update book class

--- oop/main.py
+++ oop/main.py
@@ -3,10 +3,9 @@ from book_class import Book
 def main():
   #creating instance of book
   my_book =Book(1984,"Georg Orwell",1949)
-  print( my_book)
   # Demonstrating the __str__ method
   print(__str__(my_book))
   # Demonstrating the __repr__ method
-  print(__repr__(my_book))
+  print(f"{my_book.title} by {my_book.author}, published in {my_book.year}")
   # Deleting a book instance to trigger __del_
   my_book.__del__(title)
