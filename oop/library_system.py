9e5561a884b2c74f2b32327748275d5745732e09
selamawit Haile <selamhi18@gmail.com>
2024-07-19 23:56:59 -0700

    library system

---
+++ oop/library_system.py
@@ -0,0 +1,19 @@
+# library_system.py
+class Book:
+    def __init__(self, title, author):
+        self.title  = title
+        self.author = author
+    def __str__(self):
+     return f"Book:{self.title} by {self.author}"
+class EBook(Book):
+    def __init__(self,title,author,file_size):
+        super().__init__(title,author) #call the base class __init__ method
+        self.file_size = file_size
+    def __str__(self):
+        return f"EBook:{self.title} by {self.author},File Size:{self.file_size }"
+class PrintBook(Book):
+    def __init__(self,page_count):
+     super().__init__(title,author)
+     self.page_count = page_count
+     def __str__(self):
+         return f"PrintBook: {self.title} by {self.author} ,Page Count: {self.page_count}
