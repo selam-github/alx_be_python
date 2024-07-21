be02109ba2b4c2ec3f7fe7e63099b7c43d29b80a
selamawit Haile <selamhi18@gmail.com>
2024-07-21 11:14:40 -0700

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -15,7 +15,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook:{self.title} by {self.author}, File Size: {self.file_size} KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size} KB"
 
 # Derived Class - PrintBook
 class PrintBook(Book):
@@ -24,7 +24,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook:{self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 # Composition - Library
 class Library:
